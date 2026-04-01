48537e7a24a3153fc87a3cc748edd7e225918b23
AJAX-ed <education.anayy@gmail.com>
2026-04-01 14:39:06 +0530

    Fix utils.py: rewrite showdiff - fix broken subplot logic, handle numpy arrays and PIL Images, single-image safe

--- utils.py
+++ utils.py
@@ -1,14 +1,46 @@
 from matplotlib import pyplot as plt
 from quantumimageencoding.BaseQuantumEncoder import QuantumEncoder
 from PIL import Image
-def showdiff(Encoder : QuantumEncoder, *images : Image.Image):
-    # print('MSE: ',Encoder.calculateMSE(image1, image2))
-    # try:
-    #     print('SSI: ', Encoder.calculateSSI(image1, image2))
-    # except:
-    #     pass
-    fig = plt.figure()
-    for i in range(len(images)):
-        fig.add_subplot(1,len(images),i+1)
-        plt.imshow(images[i], cmap='gray')
+import numpy
+
+def showdiff(Encoder: QuantumEncoder, *images):
+    """
+    Display one or more images side-by-side in a matplotlib figure.
+    Each item in *images may be a PIL Image, a numpy array, or a plain list.
+    Optionally computes MSE/SSI between the first two images if available.
+    """
+    if len(images) == 0:
+        print("[showdiff] No images provided.")
+        return
+
+    # Convert everything to numpy arrays for display
+    np_images = []
+    for img in images:
+        if isinstance(img, Image.Image):
+            np_images.append(numpy.array(img))
+        else:
+            np_images.append(numpy.array(img))
+
+    # Optionally print quality metrics between first two images
+    if len(np_images) >= 2:
+        try:
+            print('MSE: ', Encoder.calculateMSE(np_images[0], np_images[1]))
+        except Exception:
+            pass
+        try:
+            print('SSI: ', Encoder.calculateSSI(np_images[0], np_images[1]))
+        except Exception:
+            pass
+
+    # Plot all images side by side
+    n = len(np_images)
+    fig, axes = plt.subplots(1, n, figsize=(5 * n, 5))
+    if n == 1:
+        axes = [axes]  # make iterable when only one subplot
+    for i, (ax, img) in enumerate(zip(axes, np_images)):
+        ax.imshow(img, cmap='gray')
+        ax.set_title(f'Image {i + 1}')
+        ax.axis('off')
+    plt.suptitle('Image Comparison', fontsize=13)
+    plt.tight_layout()
     plt.show()
